fd8a757bb9ee983068768a59bc1d02f8cc0895da
thorimur <68410468+thorimur@users.noreply.github.com>
2025-08-07 14:03:35 -0400

    Revert "write_edits"
    This reverts commit 21f6ea50f3ba5437f52abe2604628e6e17db1390.

--- Mathlib/Util/Edit/Execute.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Util.Edit.Extension
-
-open Lean
-
-unsafe def main : IO Unit := do
-  initSearchPath (← getBuildDir)
-  enableInitializersExecution
-  let env ← importModules #[{module := `Mathlib}] {} (loadExts := true)
-  executeEdits env `Mathlib
