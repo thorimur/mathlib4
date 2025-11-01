0e339cd257a41ca4a09644d0c60ee890a058f64e
thorimur <thomasmurrills@gmail.com>
2025-11-01 01:12:55 -0400

    chore: update Mathlib.lean

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -159,6 +159,8 @@ import Mathlib.Tactic.Linter.DocPrime
 import Mathlib.Tactic.Linter.DocString
 import Mathlib.Tactic.Linter.FindDeprecations
 import Mathlib.Tactic.Linter.FlexibleLinter
+import Mathlib.Tactic.Linter.Foo
+import Mathlib.Tactic.Linter.Foo0
 import Mathlib.Tactic.Linter.GlobalAttributeIn
 import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.HaveLetLinter
@@ -170,6 +172,7 @@ import Mathlib.Tactic.Linter.OldObtain
 import Mathlib.Tactic.Linter.PPRoundtrip
 import Mathlib.Tactic.Linter.Style
 import Mathlib.Tactic.Linter.TextBased
+import Mathlib.Tactic.Linter.UnusedDecidable
 import Mathlib.Tactic.Linter.UnusedTactic
 import Mathlib.Tactic.Linter.UnusedTacticExtension
 import Mathlib.Tactic.Linter.UpstreamableDecl
