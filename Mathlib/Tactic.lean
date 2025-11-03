0c86b96dd33ef8e46b43f3a0d161cddde4af3df2
thorimur <68410468+thorimur@users.noreply.github.com>
2025-11-03 16:38:46 -0500

    chore: clean up Mathlib.lean etc.

--- Mathlib/Tactic.lean
+++ Mathlib/Tactic.lean
@@ -159,8 +159,6 @@ import Mathlib.Tactic.Linter.DocPrime
 import Mathlib.Tactic.Linter.DocString
 import Mathlib.Tactic.Linter.FindDeprecations
 import Mathlib.Tactic.Linter.FlexibleLinter
-import Mathlib.Tactic.Linter.Foo
-import Mathlib.Tactic.Linter.Foo0
 import Mathlib.Tactic.Linter.GlobalAttributeIn
 import Mathlib.Tactic.Linter.HashCommandLinter
 import Mathlib.Tactic.Linter.HaveLetLinter
