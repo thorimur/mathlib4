1eced6437becdba06275a0dbdf70fe043840ef27
thorimur <68410468+thorimur@users.noreply.github.com>
2026-08-12 21:09:43 -0400

    .

--- Mathlib/Cafe/Module/Meta/B.lean
+++ Mathlib/Cafe/Module/Meta/B.lean
@@ -1,12 +1,24 @@
 module
 
-import Mathlib.Cafe.Module.Meta.A
+public meta import Mathlib.Cafe.Module.Meta.A
 import Lean.Elab.Command
 
 open Lean
+
+set_option trace.Compiler.result true
+
+
+meta def barUsingBar := bar
+
+public meta def bar' := barUsingBar
+
+
+
+-- public
 run_cmd
   -- logInfo m!"{isMarkedMeta (← getEnv) ``bar}"
   -- logInfo m!"{repr <| getIRPhases (← getEnv) ``bar}"
+  logInfo m!"{isDeclMeta (← getEnv) ``bar}"
   logInfo m!"{isDeclMeta (← getEnv) ``bar}"
 
 #check foo 5
